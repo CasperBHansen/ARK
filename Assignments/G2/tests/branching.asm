1e945341236e470374fb5ae3302d6e534b7053e4
Casper B. Hansen <casperbhansen@me.com>
2013-10-20 19:58:51 +0200

    Release candidate.

--- Assignments/G2/tests/branching.asm
+++ Assignments/G2/tests/branching.asm
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 main:
 	addiu $s0, $zero, 4
 	addiu $s1, $zero, 2
@@ -18,26 +17,4 @@ next:
 	nop
 	nop
 	jal end
-	nop
-
-=======
-addiu	$s0, $zero, 4
-addiu	$s1, $zero, 2
-jal label
-	nop
-	nop
-	nop
-label:
-beq	$s0, $s1, next
-addiu	$s1, $s1, 1
-jr	$ra
-next:
-	nop
-addiu	$s2, $zero, 3
-	nop
-	nop
-jal end
-nop
->>>>>>> 54d0692b4fd9d585c14b8b91a7e161fc68def805
-end:
 	nop
