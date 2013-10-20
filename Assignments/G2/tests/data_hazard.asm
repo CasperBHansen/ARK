54d0692b4fd9d585c14b8b91a7e161fc68def805
nikolajhoyer0 <nikolaj@nh-pc.lan>
2013-10-20 19:57:41 +0200

    removed comments

--- Assignments/G2/tests/data_hazard.asm
+++ Assignments/G2/tests/data_hazard.asm
@@ -1,13 +1,8 @@
-# Her testes for data hazard stalling. 
-# Status: PASS
-# ---------------------------------------------- #
-#       Instruktion     |    Forventet output    #
-# ---------------------------------------------- #
-addiu	$s0, $zero, 3   #  s0: 3
-addiu	$s1, $zero, 4   #  s1: 4
-sw	$s0, 0($0)	#  MEM 000000: 3
-sw	$s1, 4($0)	#  MEM 000004: 4
-lw	$t1, 4($0)      # t1: 4
-lw	$t0, 0($0)      # t0: 3
-addu	$t2, $t0, $zero # t2: 3
-addu	$t3, $t1, $zero # t3: 4
+addiu	$s0, $zero, 3
+addiu	$s1, $zero, 4
+sw	$s0, 0($0) # MEM 000000: 3
+sw	$s1, 4($0) # MEM 000004: 4
+lw	$t1, 4($0)
+lw	$t0, 0($0)
+addu	$t2, $t0, $zero
+addu	$t3, $t1, $zero
