df93459c79febb5dc1b7fe6e4f159952700a920e
nikolajhoyer0 <nikolaj@nh-pc.lan>
2013-10-20 19:46:18 +0200

    mem

--- Assignments/G2/tests/memory.asm
+++ Assignments/G2/tests/memory.asm
@@ -1,11 +1,4 @@
-# Her testes for hukommelsesoperationerne sw og lw. 
-# Status: PASS
-# ---------------------------------------------- #
-#       Instruktion     |    Forventet output    #
-# ---------------------------------------------- #
-addiu	$s0, $zero, 3   #  s0: 3
-addiu	$s1, $zero, 4   #  s1: 4
-sw	$s0, 0($0)	#  MEM 000000: 3
-sw	$s1, 4($0)	#  MEM 000004: 4
-lw	$t0, 0($0)	#  t0: 3
-lw	$t1, 4($0)	#  t1: 4
+addiu $s0, $zero, 4
+sw $s0, 0($0)
+lw $s1, 0($0)
+sw $s2, 4($0)
