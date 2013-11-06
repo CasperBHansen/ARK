8f7ff8a41bdecb67992b18f6bdcecc14c36c9364
Casper B. Hansen <casperbhansen@me.com>
2013-11-06 14:40:33 +0100

    Added the beginnings of a disposition.

--- Assignments/G2/tests/register_forwarding.asm
+++
@@ -1,8 +0,0 @@
-# Her testes for EXE forwarding
-# Status: PASS
-# ---------------------------------------------- #
-#       Instruktion     |    Forventet output    #
-# ---------------------------------------------- #
-addiu $s0, $zero, 2    # s0: 2
-addiu $s1, $zero, 3    # s1: 3
-addu $s2, $s0, $s1     # s2: 5
